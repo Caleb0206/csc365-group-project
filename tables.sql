1fae8c696f775bdc666eb1e450a9cc153681f5e0
calebhuang <calebhuang0206@gmail.com>
2025-02-12 15:12:05 -0800

    added tables.sql again

---
+++ tables.sql
@@ -0,0 +1,22 @@
+create table Artist (
+	aid INT auto_increment primary key,
+    aname VARCHAR(30) NOT NULL
+);
+create table Song (
+	sid INT auto_increment primary key,
+    sname VARCHAR(30) NOT NULL,
+    album VARCHAR(30) NOT NULL,
+    genre ENUM(
+		'pop','hip-hop','rap','country','reggae',
+        'r&b','folk','blues','edm','classical',
+        'rock','jazz','metal','punk','latin',
+        'kpop','other'),
+	length INT NOT NULL
+);
+create table Performs (
+	aid INT,
+	sid INT,
+    primary key(aid, sid),
+    foreign key(aid) references Artist(aid),
+    foreign key(sid) references Song(sid)
+);
